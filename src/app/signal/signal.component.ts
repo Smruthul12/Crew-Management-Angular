653dc208feef786268f43185c09c2298812d1d98
Smruthul12 <msmruthul2002@gmail.com>
2024-09-28 15:56:17 +0530

    signals done

--- src/app/signal/signal.component.ts
+++ src/app/signal/signal.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, Injector, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal',
@@ -21,5 +21,33 @@ export class SignalComponent {
     title: 'Ready Player One',
     descripition:'Futuristic Movie about gaming'
 });
+
+  setCounter(){
+    this.counter.set(30);
+  }
+
+  setVideo(){
+    this.videos.set({
+      "title":"New Title",
+      "descripition": "New Description"
+    })
+  }
+
+  updateCounter(){
+    this.counter.update(counter => counter +50);
+  }
+
+  constructor(private injector: Injector){
+    
+  }
+
+  counterEffect(): void{
+    effect(() => {
+      console.log("Value of counter" + this.counter());
+      console.log("Value of counter" + this.videos().title);
+    },{injector: this.injector})
+  }
+
+
 
 }
